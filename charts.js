7aecda86657e04e19e2107f09631067c717471d3
Freek1995 <49745978+Freek1995@users.noreply.github.com>
2019-05-03 11:56:00 +0200

    0305

--- charts.js
+++ charts.js
@@ -1,7 +1,7 @@
 //setting the axis and rectangles for bar chart 12-factor scale
 
 var margin = {top: 50, right: 30, bottom: 240, left: 170},
-    width = 1070 - margin.left - margin.right,
+    width = 850 - margin.left - margin.right,
     height = 450 - margin.top - margin.bottom;
 
 var x = d3.scale.ordinal()
@@ -287,7 +287,7 @@ var  mouseout_legend_fac = function(d) {
 
 // setting the 4-cateogory bar chart axis 
 var margin_cat = {top_cat: 50, right_cat: 30, bottom_cat: 240, left_cat: 170},
-    width_cat = 1070 - margin_cat.left_cat - margin_cat.right_cat,
+    width_cat = 850 - margin_cat.left_cat - margin_cat.right_cat,
     height_cat = 450 - margin_cat.top_cat - margin_cat.bottom_cat;
 
 var x_cat= d3.scale.ordinal()
@@ -563,7 +563,7 @@ var  mouseout_legend_fac = function(d) {
 };
 
 var marginmacc = { topmacc: 20, rightmacc: 20, bottommacc: 30, leftmacc: 40 },
-widthmacc  = 1070 - marginmacc.leftmacc - marginmacc.rightmacc,
+widthmacc  = 850 - marginmacc.leftmacc - marginmacc.rightmacc,
 heightmacc = 550 - marginmacc.topmacc - marginmacc.bottommacc;
 
 // Add the visualization svg canvas to the vis-container <div>
